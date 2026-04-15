a206d426b0a619682abff15b8ee7b683cbfc72ee
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-15 00:13:19 -0700

    Starting lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+const addBurger = () => {
+  let newBurger = "Flatburger";
+};
